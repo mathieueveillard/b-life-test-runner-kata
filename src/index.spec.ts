9e94e49fe26d4cee574bde1872832169b18e13c3
mathieueveillard <mathieu.eveillard@gmail.com>
2025-09-02 19:06:05 +0200

    Gist, slightly improved

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,11 +1,17 @@
 import { test } from ".";
 
-test("A simple test that succeeds", (expectEquals) => {
-  return expectEquals(0, 0);
+test("A simple test that succeeds", (expect) => {
+  return expect(0).equals(0);
 });
 
-test("A simple test that fails", (expectEquals) => {
-  return expectEquals(0, 1);
+test("A simple test that fails", (expect) => {
+  return expect(0).equals(1);
+});
+
+test("Expecting that a function throws an error", (_, expect) => {
+  return expect(() => {
+    throw Error("Oops, I did it again!");
+  }).throws();
 });
 
 // npx ts-node src/index.spec.ts
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,36 @@
-type ExpectationFunction = <T>(expected: T, actual: T) => boolean;
+type ExpectFunction = <T>(expected: T) => {
+  equals: (actual: T) => boolean;
+};
 
-type TestCallback = (expect: ExpectationFunction) => boolean;
+type ExpectThrowsFunction = (fn: () => void) => {
+  throws: () => boolean;
+};
+
+type TestCallback = (expect: ExpectFunction, expectThrows: ExpectThrowsFunction) => boolean;
 
 const displayStatus = (result: boolean): string => {
   if (result) {
     return "🟢";
   }
-
   return "🔴";
 };
 
+const expect: ExpectFunction = <T>(expected: T) => ({
+  equals: (actual: T) => actual === expected,
+});
+
+const expectThrows: ExpectThrowsFunction = (fn) => ({
+  throws: () => {
+    try {
+      fn();
+      return false;
+    } catch (error) {
+      return true;
+    }
+  },
+});
+
 export const test = (description: string, scenario: TestCallback): void => {
-  const expect: ExpectationFunction = (expected, actual) => actual === expected;
-  const result = scenario(expect);
+  const result = scenario(expect, expectThrows);
   console.log(`${displayStatus(result)} ${description}`);
 };
